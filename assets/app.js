e6e93962e01fc1171c6cf41c7918760de3a43f96
corentinT <tailleur.corentin27@gmail.om>
2021-04-06 19:17:23 +0200

    Ajout du header

--- assets/app.js
+++ assets/app.js
@@ -8,7 +8,8 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 import 'semantic-ui-css/semantic.min.css';
-
+import 'semantic-ui-css/components/dropdown.min.css';
 // start the Stimulus application
 import './bootstrap';
 import 'semantic-ui-css/semantic.min'
+import 'semantic-ui-css/components/dropdown.min';
---
+++ src/Twig/CategoryExtension.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App\Twig;
+
+
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class CategoryExtension extends AbstractExtension
+{
+    private CategoryRepository $categoryRepository;
+
+    public function __construct(CategoryRepository $categoryRepository)
+    {
+        $this->categoryRepository = $categoryRepository;
+    }
+
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('getCategories', [$this,'categories'])
+        ];
+    }
+
+    /**
+     * @return Category[]
+     */
+    public function categories(): array
+    {
+        return $this->categoryRepository->findAll();
+    }
+}
