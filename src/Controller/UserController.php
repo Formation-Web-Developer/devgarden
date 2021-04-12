bba9b2bd5a40a1624738c5c86a857f8b83750fd0
corentinT <tailleur.corentin27@gmail.om>
2021-04-12 14:12:42 +0200

    Ajout de la page profil d'un utilisateur

---
+++ src/Controller/UserController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/utilisateurs", name="user_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
+
+    /**
+     * @Route("/utilisateurs/profil", name="user_profil", priority=5)
+     * @IsGranted("ROLE_USER")
+     */
+    public function profile():Response
+    {
+        return $this->showUser($this->getUser());
+    }
+
+    private function showUser(User $user): Response
+    {
+        return $this->render('user/show.html.twig', [
+            'user' => $user,
+            'self' => $this->getUser() && $this->getUser()->getId() === $user->getId()
+        ]);
+    }
+
+    /**
+     * @Route("/utilisateurs/{name}", name="user", priority="3")
+     */
+    public function user(User $user):Response
+    {
+        return $this->showUser($user);
+    }
+}
